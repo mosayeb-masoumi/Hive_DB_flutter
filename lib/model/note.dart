1da90c36eb4bffe84ed1224308fb2b8adaf3925c
Mosayeb Masoumi <masoumi.projects.co@gmail.com>
2023-02-27 18:14:06 +0330

    hive db

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -18,6 +18,13 @@ class Note extends HiveObject {
 
 
 // Generate the NoteAdapter for your note model class:
+
+// add below dependencies in dev_dependencies
+// dev_dependencies:
+// flutter_test:
+// sdk: flutter
+// hive_generator: ^2.0.0
+// build_runner: ^2.3.3
 //
 // run below code in terminal   the NoteAdapter will be created automatically
 // Copy code
